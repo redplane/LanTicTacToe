8ce4162b8b36490d6d0088890d3d14fb01d79567
redplane <redplane@users.noreply.github.com>
2015-12-27 06:28:51 +0700

    - Change GUI folder to Views

---
+++ S-SOURCE_CODE/ClientLayer/Views/Dialogs/CreateGameDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ClientLayer.Views.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for CreateGameDialog.xaml
+    /// </summary>
+    public partial class CreateGameDialog : Window
+    {
+        public CreateGameDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
